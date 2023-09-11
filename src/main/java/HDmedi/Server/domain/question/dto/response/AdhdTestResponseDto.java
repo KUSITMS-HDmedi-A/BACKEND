05fc36a4c9e517f372488426d3165dba31b3ad77
seungryeol <opop0421@naver.com>
2023-09-11 17:33:42 +0900

    [feat] 설문 API, 설문 결과 API 구현 완료

---
+++ src/main/java/HDmedi/Server/domain/question/dto/response/AdhdTestResponseDto.java
@@ -0,0 +1,16 @@
+package HDmedi.Server.domain.question.dto.response;
+
+import HDmedi.Server.domain.user_child.dto.response.ResponseDto;
+import lombok.*;
+
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdhdTestResponseDto extends ResponseDto {
+
+    String[] question;
+
+
+}
