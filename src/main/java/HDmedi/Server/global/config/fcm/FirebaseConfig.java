40e52e221ff9eea425ead2756e5e27540563566c
versatile0010 <siam0294@naver.com>
2023-09-17 03:05:23 +0900

    feat: firebase config

--- src/main/java/HDmedi/Server/global/config/fcm/FirebaseConfig.java
+++ src/main/java/HDmedi/Server/global/config/fcm/FirebaseConfig.java
@@ -1,16 +1,19 @@
 package HDmedi.Server.global.config.fcm;
 
 import HDmedi.Server.global.exception.badrequest.FirebaseConfigException;
+import HDmedi.Server.global.exception.badrequest.SDKNotFoundException;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
 
 @Slf4j
 @Service
@@ -19,7 +22,10 @@ public class FirebaseConfig {
     @PostConstruct
     public void init() {
         try {
-            FileInputStream resource = new FileInputStream("./src/main/resources/serviceAccountKey.json");
+            String path = "./src/main/resources/serviceAccountKey.json";
+
+            FileInputStream resource = new FileInputStream(path);
+            BufferedReader br = new BufferedReader(new InputStreamReader(resource));
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(
                             GoogleCredentials.fromStream(
@@ -28,6 +34,9 @@ public class FirebaseConfig {
                     .build();
             FirebaseApp.initializeApp(options);
             log.info("firebase application init complete");
+        } catch (FileNotFoundException e) {
+            log.warn("[ 오류 ] firebase SDK json file is not found!! check your path....");
+            throw new SDKNotFoundException();
         } catch (Exception e) {
             log.warn("[ 오류 ] firebase application init failed!!");
             throw new FirebaseConfigException();
