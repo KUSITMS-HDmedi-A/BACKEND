9fc6f6c240ae031f9216dc1aaa05c8b2c2f03ab3
versatile0010 <siam0294@naver.com>
2023-09-16 20:20:03 +0900

    feat: set env

--- src/main/java/HDmedi/Server/global/config/FCM/FirebaseConfig.java
+++ src/main/java/HDmedi/Server/global/config/FCM/FirebaseConfig.java
@@ -5,33 +5,35 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileReader;
+import java.io.*;
 
 @Slf4j
 @Component
 public class FirebaseConfig {
+
+    @Value("${fcm.account}")
+    private String account;
     @PostConstruct
     public void init() {
         try {
-            BufferedReader br = new BufferedReader(
-                    new FileReader("src/main/resources/serviceAccountKey.json"));
-            while (true) {
-                String str = br.readLine();
-                if (str == null) break;
-                System.out.println(str);
-            } br.close();
+//            BufferedReader br = new BufferedReader(
+//                    new FileReader("src/main/resources/serviceAccountKey.json"));
+//            while (true) {
+//                String str = br.readLine();
+//                if (str == null) break;
+//                System.out.println(str);
+//            } br.close();
 
-
-            FileInputStream serviceAccount =
-                    new FileInputStream("src/main/resources/serviceAccountKey.json");
+            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(account.getBytes());
+            //FileInputStream serviceAccount =
+              //      new FileInputStream("src/main/resources/serviceAccountKey.json");
             FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setCredentials(GoogleCredentials.fromStream(byteArrayInputStream))
                     .build();
             FirebaseApp.initializeApp(options);
             log.info("firebase application init complete");
