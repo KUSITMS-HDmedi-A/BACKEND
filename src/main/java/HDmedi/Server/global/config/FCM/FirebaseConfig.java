c0341220ee5a6a6f0feb0e76b408dd1c3260efbc
JaeHyeon Lee <siam0294@naver.com>
2023-09-16 19:47:24 +0900

    fix: add logging for fcm (#13)
    * feat: add log
    
    * fix: logging for fcm

--- src/main/java/HDmedi/Server/global/config/FCM/FirebaseConfig.java
+++ src/main/java/HDmedi/Server/global/config/FCM/FirebaseConfig.java
@@ -6,16 +6,28 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileReader;
 
 @Slf4j
-@Configuration
+@Component
 public class FirebaseConfig {
     @PostConstruct
     public void init() {
         try {
+            BufferedReader br = new BufferedReader(
+                    new FileReader("src/main/resources/serviceAccountKey.json"));
+            while (true) {
+                String str = br.readLine();
+                if (str == null) break;
+                System.out.println(str);
+            } br.close();
+
+
             FileInputStream serviceAccount =
                     new FileInputStream("src/main/resources/serviceAccountKey.json");
             FirebaseOptions options = new FirebaseOptions.Builder()
