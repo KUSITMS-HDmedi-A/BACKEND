dc583ddc5aca2acae14be9a7bb2fcef15a030331
JaeHyeon Lee <siam0294@naver.com>
2023-09-17 16:45:06 +0900

    Feature/alarm (#19)
    * feat: add log
    
    * fix: logging for fcm
    
    * feat: set env
    
    * fix: fcm bug
    
    * feat: small bug fix
    
    * feat: 유저 구성원 간단 조회 API
    
    * feat: security config
    
    * feat: send fcm push

--- src/main/java/HDmedi/Server/fcm/dto/FCMNotificationRequestDto.java
+++ src/main/java/HDmedi/Server/fcm/dto/FCMNotificationRequestDto.java
@@ -7,13 +7,11 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class FCMNotificationRequestDto {
-    private Long targetUserId; // 푸쉬 알람을 보낼 유저 아이디
     private String title; // 푸쉬 알람 제목
     private String body; // 푸쉬 알람 내용
 
     @Builder
-    public FCMNotificationRequestDto(Long targetUserId, String title, String body) {
-        this.targetUserId = targetUserId;
+    public FCMNotificationRequestDto(String title, String body) {
         this.title = title;
         this.body = body;
     }
