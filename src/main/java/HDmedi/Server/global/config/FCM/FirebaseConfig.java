5c2b486f5b4ced9568ff877d1bc125b0f435160d
versatile0010 <siam0294@naver.com>
2023-09-17 17:22:02 +0900

    feat: fix json bug

--- src/main/java/HDmedi/Server/global/config/FCM/FirebaseConfig.java
+++ src/main/java/HDmedi/Server/global/config/FCM/FirebaseConfig.java
@@ -1,38 +1,71 @@
-package HDmedi.Server.global.config.FCM;
+package HDmedi.Server.global.config.fcm;
 
 import HDmedi.Server.global.exception.badrequest.FirebaseConfigException;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
+import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
+import javax.annotation.PostConstruct;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+import org.springframework.core.io.ClassPathResource;
 
 import javax.annotation.PostConstruct;
 import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
-import java.util.List;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-//@Slf4j
-//@Service
-//public class FirebaseConfig {
-//
-//
-//    @PostConstruct
-//    public void init() {
-//        try {
-//            FileInputStream serviceAccount = new FileInputStream("./src/main/resources/serviceAccountKey.json");
-//            FirebaseOptions options = new FirebaseOptions.Builder()
-//                    .setCredentials(
-//                            GoogleCredentials.fromStream(
-//                                    serviceAccount
-//                            ))
-//                    .build();
-//            FirebaseApp.initializeApp(options);
-//            log.info("firebase application init complete");
-//        } catch (Exception e) {
-//            log.warn("[ 오류 ] firebase application init failed!!");
-//            throw new FirebaseConfigException();
-//        }
-//    }
-//}
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class FirebaseConfig {
+
+    @PostConstruct
+    public void initialize() throws IOException {
+        // serviceAccountKey.json 파일을 ClassPath에서 읽어옴
+        ClassPathResource resource = new ClassPathResource("serviceAccountKey.json");
+
+        try {
+            // InputStream에서 데이터를 읽음
+            byte[] jsonData = IOUtils.toByteArray(resource.getInputStream());
+
+            // JSON 파서 생성 및 Lenient 모드 설정
+            JsonReader jsonReader = new JsonReader(new InputStreamReader(new ByteArrayInputStream(jsonData)));
+            jsonReader.setLenient(true);
+
+            // GoogleCredentials 생성
+            GoogleCredentials credentials = GoogleCredentials.fromStream(new ByteArrayInputStream(jsonData));
+
+            // FirebaseOptions 빌더를 사용하여 FirebaseOptions 생성
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(credentials)
+                    .build();
+
+            // FirebaseApp이 초기화되지 않았을 때만 초기화
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+                log.info("FirebaseApp initialization complete");
+            }
+        } catch (IOException e) {
+            log.error("Error initializing FirebaseApp", e);
+        }
+    }
+}
